3fc6b3f16b6bc6e11f10c761a67730d06a3ed5f0
Adam <adam.kjall@hotmail.com>
2018-12-01 11:14:26 +0100

    reupload lost code

--- BaitBoat_v2/app/src/main/java/com/adamk/baitboat_v2/MainActivity.java
+++ BaitBoat_v2/app/src/main/java/com/adamk/baitboat_v2/MainActivity.java
@@ -3,32 +3,106 @@ package com.adamk.baitboat_v2;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import org.osmdroid.config.Configuration;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Map map;
 
+    Button buttonLeft;
+    Button buttonRight;
+    EditText txtAddress;
+    Socket myAppSocket = null;
+
+    public static String wifiModuleIp = "";
+    public static int wifiModulePort = 0;
+    public static String CMD = "0";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkLocationPermissions();
+        //checkLocationPermissions();
 
         // load/initialize the osmdroid configuration
-        final Context ctx = getApplicationContext();
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        //final Context ctx = getApplicationContext();
+        //Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 
+        buttonLeft = (Button) findViewById(R.id.button_left);
+        buttonRight = (Button) findViewById(R.id.button_right);
+
+        txtAddress = (EditText) findViewById(R.id.ipAddress);
+
+        buttonLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getIPandPort();
+                CMD = "Left";
+                Socket_AsyncTask task = new Socket_AsyncTask();
+                task.execute();
+            }
+        });
+        buttonRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getIPandPort();
+                CMD = "Right";
+                Socket_AsyncTask task = new Socket_AsyncTask();
+                task.execute();
+            }
+        });
 
     }
+    public void getIPandPort()
+    {
+        String iPandPort = txtAddress.getText().toString();
+        Log.d("MYTEST","IP String: "+ iPandPort);
+        String temp[]= iPandPort.split(":");
+        wifiModuleIp = temp[0];
+        wifiModulePort = Integer.valueOf(temp[1]);
+        Log.d("MY TEST","IP:" +wifiModuleIp);
+        Log.d("MY TEST","PORT:"+wifiModulePort);
+    }
+
+
+    public class Socket_AsyncTask extends AsyncTask<Void,Void,Void>
+    {
+        Socket socket;
+
+        @Override
+        protected Void doInBackground(Void... params){
+            try{
+                InetAddress inetAddress = InetAddress.getByName(MainActivity.wifiModuleIp);
+                socket = new java.net.Socket(inetAddress,MainActivity.wifiModulePort);
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeBytes(CMD);
+                dataOutputStream.close();
+                socket.close();
+            }catch (UnknownHostException e){e.printStackTrace();}catch (IOException e){e.printStackTrace();}
+            return null;
+        }
+    }
+
+
 
     /**
      * Prompts the user for location permissions
@@ -56,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        //map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
     /**
@@ -68,6 +142,6 @@ public class MainActivity extends AppCompatActivity {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
-        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        //map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 }
